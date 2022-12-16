1bacbf2e20337487b83eb075f06587635df8a003
ZoopSoup <zoopidysoupidy@gmail.com>
2022-12-16 11:41:41 -0800

    Added Bird Script
    Created Script for Bird and inputted some code into it.

---
+++ FlappyBirdFinalVillanuevaP4/Assets/Scripts/Bird.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public float upForce;
+    private bool isDead = false;
+
+    private Animator anim;
+    private Rigidbody2D rb2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
