c2972bac8d074ac9d66ac734f88f76bf8c727ef4
ZoopSoup <zoopidysoupidy@gmail.com>
2022-12-20 23:01:43 -0800

    Made It to Step 8/10 In Flappy Bird Project
    Got To the Middle of Step 8/10 and had some errors on steps 6&7.

--- FlappyBirdFinalVillanuevaP4/Assets/Scripts/Bird.cs
+++ FlappyBirdFinalVillanuevaP4/Assets/Scripts/Bird.cs
@@ -8,11 +8,13 @@ public class Bird : MonoBehaviour
 
     private bool isDead = false;
     private Rigidbody2D rb2d;
+    private Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator> ();
     }
 
     // Update is called once per frame
@@ -23,13 +25,17 @@ public class Bird : MonoBehaviour
             if (Input.GetMouseButtonDown(0))
             {
                 rb2d.velocity = Vector2.zero;
-                rb2d.AddForce(new Vector2(0, upForce));
+                rb2d.AddForce(new Vector2(0f, 200f));
+                anim.SetTrigger ("Flap");
             }
         }
     }
 
     void OnCollisionEnter2D()
     {
+        rb2d.velocity = Vector2.zero;
         isDead = true;
+        anim.SetTrigger ("Die");
+        GameControl.instance.BirdDied ();
     }
 }
